e2cc86c4b064cfaedb25cc37d6e13ac860a1c0e0
Alex Sparrow <alexsparrow5973@gmail.com>
2022-11-05 21:41:10 +0300

    updated views single_news, added redirect_target

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,6 +1,11 @@
-from flask import abort, Blueprint, current_app, render_template
+from flask import abort, Blueprint, flash, current_app,\
+    render_template, redirect
+from flask_login import current_user, login_required
+from webapp.db import db
 from webapp.news.models import Comment, News
+from webapp.news.forms import CommentForm
 from webapp.weather import weather_by_city
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('news', __name__)
 
@@ -9,7 +14,8 @@ blueprint = Blueprint('news', __name__)
 def index():
     title = "Новости Python"
     weather = weather_by_city(current_app.config['WEATHER_DEFAULT_CITY'])
-    news_list = News.query.filter(News.text.isnot(None)).order_by(News.published.desc()).all()
+    news_list = News.query.filter(News.text.isnot(None)).\
+        order_by(News.published.desc()).all()
     return render_template(
         'news/index.html',
         page_title=title,
@@ -23,15 +29,33 @@ def single_news(news_id):
     my_news = News.query.filter(News.id == news_id).first()
     if not my_news:
         abort(404)
-    comment_form = Comment(news_id=my_news.id)
+    form = CommentForm(news_id=my_news.id)
     return render_template(
         'news/single_news.html',
         page_title=my_news.title,
         news=my_news,
-        comment_form=comment_form,
+        comment_form=form,
         )
 
 
 @blueprint.route('/news/comment', methods=['POST'])
+@login_required
 def add_comment():
-    pass
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(
+            text=form.comment_text.data,
+            news_id=form.news_id.data,
+            user_id=current_user.id
+        )
+        db.session.add(comment)
+        db.session.commit()
+        flash('Комментарий успешно добавлен')
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в заполнении поля "{}": - {}'.format(
+                    getattr(form, field).label.text,
+                    error
+                ))
+    return redirect(get_redirect_target())
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_user, logout_user
 from webapp.db import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
 
@@ -12,10 +13,14 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 def login():
     if current_user.is_authenticated:
         flash('Вы уже авторизовались')
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
     title = "Авторизация"
     login_form = LoginForm()
-    return render_template('user/login.html', page_title=title, form=login_form)
+    return render_template(
+            'user/login.html',
+            page_title=title,
+            form=login_form
+        )
 
 
 @blueprint.route('/process-login', methods=['POST'])
@@ -26,7 +31,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
         flash('Неправильное имя пользователя или пароль')
         return redirect(url_for('user.login'))
 
@@ -45,14 +50,22 @@ def registr():
         return redirect(url_for('news.index'))
     title = "Регистрация"
     form = RegistrationForm()
-    return render_template('user/registration.html', page_title=title, form=form)
+    return render_template(
+            'user/registration.html',
+            page_title=title,
+            form=form
+        )
 
 
 @blueprint.route('/process-reg', methods=['POST'])
 def process_reg():
     form = RegistrationForm()
     if form.validate_on_submit():
-        new_user = User(username=form.username.data, mail=form.email.data, role='user')
+        new_user = User(
+            username=form.username.data,
+            mail=form.email.data,
+            role='user',
+        )
         new_user.set_password(form.password.data)
         db.session.add(new_user)
         db.session.commit()
@@ -61,5 +74,6 @@ def process_reg():
     else:
         for field, errors in form.errors.items():
             for error in errors:
-                flash(f'В поле {getattr(form, field).label.text} ошибка,\n {error}')
+                flash(f'В поле {getattr(form, field).label.text}\
+                    ошибка,\n {error}')
         return redirect(url_for('user.registr'))
